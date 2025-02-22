895d94940cb96d556490e812c32cb5bd7472d47e
AsTech-900 <sallu@gmail.com>
2025-02-23 00:44:19 +0600

    updates files

---
+++ README.md
@@ -0,0 +1,23 @@
+Select Menu Dropdown
+
+This is a simple custom select menu that allows users to select a social media platform from a dropdown. When a user clicks a div containing an image and paragraph, the selected platform is displayed at the top of the menu.
+
+Features
+* Custom dropdown menu
+* Click to toggle visibility
+* Displays selected option
+* Smooth icon rotation animation
+
+Technologies Used
+
+* HTML
+* CSS
+* JavaScript
+
+How It Works
+
+Clicking the select field toggles the visibility of the dropdown menu.
+When an option is selected, its text appears in the display field.
+The dropdown hides after selection, and the icon rotates for a smooth UI effect.
+
+Happy Coding!
---
+++ app.js
@@ -0,0 +1,28 @@
+const displayEl = document.getElementById("display");
+let iconEl = document.getElementById("icon");
+let selectField = document.querySelector(".select-field");
+let socialMediaDiv = document.querySelector(".social-media")
+let app = document.querySelectorAll('#apps')
+let appArr = Array.from(app)
+
+
+selectField.addEventListener("click",()=>{
+    if (!socialMediaDiv.classList.contains("visible")) {
+        iconEl.classList.add("rotateicon");
+        socialMediaDiv.classList.add("visible");
+    }else{
+        iconEl.classList.remove("rotateicon");
+        socialMediaDiv.classList.remove("visible");
+        
+    };   
+});
+appArr.forEach(el =>{
+    el.addEventListener("click",function(){
+        let para = this.querySelector('p').innerHTML;
+        displayEl.innerHTML = para;
+        socialMediaDiv.classList.remove("visible");
+        iconEl.classList.remove("rotateicon");
+        
+    });
+    
+});
